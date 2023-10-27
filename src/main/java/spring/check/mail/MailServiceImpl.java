79365eedce547ddb16bcad150f7bccab817f6a3c
yeaji862 <kimy_j_@naver.com>
2023-10-27 18:10:33 +0900

    mail sand text 수정

--- src/main/java/spring/check/mail/MailServiceImpl.java
+++ src/main/java/spring/check/mail/MailServiceImpl.java
@@ -20,13 +20,15 @@ public class MailServiceImpl implements MailService {
     public boolean sendMail(String userId, String random_id, int userNum) {
         log.info("EmailService sendMail()");
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        String mailText = "안녕하세요! check 입니다.<br>\n" +
+                "비밀번호를 변경하시려면 <a href=\"http://localhost:8080/edit/"+random_id+"?userNum="+userNum+"\">여기를</a> 눌러주세요.<br>\n" +
+                "링크 유효시간은 24시간입니다.";
 
         try {
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
             mimeMessageHelper.setTo(userId); // 메일 수신자
             mimeMessageHelper.setSubject("<Check> 비밀번호 재설정"); // 메일 제목
-            mimeMessageHelper.setText("안녕하세요! 링크를 클릭하여 비밀번호를 재설정 해주세요. <br> http://localhost:8080/edit/"+random_id+"?userNum="+userNum+
-                    "<br> 유효시간은 24시간입니다.", true); // 메일 본문 내용, HTML 여부
+            mimeMessageHelper.setText(mailText, true); // 메일 본문 내용, HTML 여부
             javaMailSender.send(mimeMessage);
             return true;
         } catch (MessagingException e) {
